c0f1616e78ae5463031b77aa73f9d8bebbc07563
Asreds Sebangsa <asih.priyatno@sebangsa.com>
2017-01-09 10:11:13 +0700

    add control from keyboard

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 window.startGame = function startGame() {
     myGameArea.start();
-    myGamePiece = new component(30, 30, "red", 200, 440);
+    myGamePiece = new component(50, 60, 'assets/img/balon.png', 180, 410, 'image');
 }
 
 var myGameArea = {
@@ -11,6 +11,15 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.getElementById('canvas').insertBefore(this.canvas, document.getElementById('canvas').childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
+        
+        //handle keyboard
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        });
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+            clearmove();
+        });
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
--- script/component.js
+++ script/component.js
@@ -4,26 +4,51 @@
 */
 
 
-function component(width, height, color, x, y) {
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
     this.width = width;
     this.height = height;
     this.speedX = 0;
     this.speedY = 0;
     this.x = x;
-    this.y = y; 	
+    this.y = y;
     this.update = function(){
         ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        if (type == "image") {
+          ctx.drawImage(this.image, 
+            this.x, 
+            this.y,
+            this.width, this.height);
+        } else {
+          ctx.fillStyle = color;
+          ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     this.newPos = function() {
-        this.x += this.speedX;
-        this.y += this.speedY;        
+        // limit let and rigt component on canvas
+        if ((this.speedX === 1 && this.x <=360) || (this.speedX === -1 && this.x>2)) {
+            this.x += this.speedX;
+        }
+
+        if ((this.y <=468 && this.speedY === 1) || this.speedY === -1) {
+            this.y += this.speedY;
+        }
     }   
 }
 
 function updateGameArea() {
     myGameArea.clear();
+
+    //update from keyboard
+    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
+    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
+    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+
     myGamePiece.newPos();  
     myGamePiece.update();
 }
@@ -43,6 +68,7 @@ window.moveleft = function moveleft() {
 
 window.moveright = function moveright() {
     myGamePiece.speedX += 1; 
+    console.log(myGamePiece.x)
 }
 
 window.clearmove = function clearmove() {
