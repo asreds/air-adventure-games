cd348e0e682e6b5fe31f79fad7cd223772acd323
Asreds Sebangsa <asih.priyatno@sebangsa.com>
2017-01-10 10:34:23 +0700

    Add score games

--- script/app.js
+++ script/app.js
@@ -1,6 +1,11 @@
-window.startGame = function startGame() {
-    myGameArea.start();
-    myGamePiece = new component(50, 60, 'assets/img/balon.png', 180, 410, 'image');
+var myGamePiece;
+var myObstacles = [];
+var myScore;
+
+window.startGame = function startGame () {
+  myGamePiece = new component(50, 60, 'assets/img/airplane.png', 180, 200, 'image');
+  myScore = new component("30px", "Consolas", "black", 20, 40, "text");
+  myGameArea.start();
 }
 
 var myGameArea = {
@@ -9,9 +14,10 @@ var myGameArea = {
         this.canvas.width = 400;
         this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
+        this.frameNo = 0;
         document.getElementById('canvas').insertBefore(this.canvas, document.getElementById('canvas').childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
-        
+
         //handle keyboard
         window.addEventListener('keydown', function (e) {
             myGameArea.key = e.keyCode;
@@ -23,7 +29,8 @@ var myGameArea = {
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
     }
 }
-
-
